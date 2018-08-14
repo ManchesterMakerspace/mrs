4472b82d7c7913994448d9b13104e6ea1c356a7d
PaulBeaudet <inof8or@gmail.com>
2018-08-14 15:51:48 -0400

    maybe some hello world code

--- mrs.js
+++ mrs.js
@@ -1,1 +1,15 @@
 // mrs.js Member Retention Service ~ Copyright 2018 Paul Beaudet ~ MIT License
+// 'use strict';
+
+module.exports.remember = function(event, context, callback) {
+    console.log(JSON.parse(event.body)); // Contains incoming request data (e.g., query params, headers and more)
+    var response = {
+      statusCode: 200,
+      headers: {
+        "x-custom-header" : "My Header Value"
+      },
+      body: JSON.stringify({ "message": "Hello World!" })
+    };
+
+    callback(null, response);
+};

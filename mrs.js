2d618bc86b126d305cb119aa07ebf083805128f4
PaulBeaudet <inof8or@gmail.com>
2018-08-15 17:26:06 -0400

    Figuring if we can speak json

--- mrs.js
+++ mrs.js
@@ -1,14 +1,31 @@
 // mrs.js Member Retention Service ~ Copyright 2018 Paul Beaudet ~ MIT License
 // 'use strict';
 
+var crypto = require('crypto');        // verify request from slack is from slack with hmac-256
+var fs = require('fs');                // i dunno, I like files, and chickens
+var bParser = require('body-parser');  // read incoming request bodies AKA &+% diliminated weirdness in api request, maybe this is just for middleware?
+
 module.exports.remember = function(event, context, callback) {
     var response = {
-      statusCode: 200,
-      headers: {
-        "x-custom-header" : "My Header Value"
-      },
-      body: event.body.text
+        statusCode: 200,
+        headers: {
+            'Content-type': 'application/json'
+        },
+        body: {
+            'text': 'hello world',
+            'attachments': {
+                'text': 'yo this in an attachment'
+            }
+        }
     };
+    callback(null, response); // do the thing
+    console.log(event.body);  // would rather have this crash after making a response
+};
 
-    callback(null, response);
+
+var varify = {
+    slack_sign_secret: process.env.SLACK_SIGNING_SECRET,
+    request: function(event){
+
+    }
 };

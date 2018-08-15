cdfdcf9f2007c548293d0cd1521d26bb4f8563d4
PaulBeaudet <inof8or@gmail.com>
2018-08-15 16:25:48 -0400

    another test

--- mrs.js
+++ mrs.js
@@ -2,13 +2,12 @@
 // 'use strict';
 
 module.exports.remember = function(event, context, callback) {
-    console.log(event); // Contains incoming request data (e.g., query params, headers and more)
     var response = {
       statusCode: 200,
       headers: {
         "x-custom-header" : "My Header Value"
       },
-      body: JSON.stringify({ "message": "Hello World!" })
+      body: event.body.text
     };
 
     callback(null, response);

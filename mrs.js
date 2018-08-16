a43dab3ad0e949ee74e2bfae0a4662c5d2020125
PaulBeaudet <inof8or@gmail.com>
2018-08-16 15:05:08 -0400

    varify request from slack

--- mrs.js
+++ mrs.js
@@ -2,29 +2,33 @@
 // 'use strict';
 
 var crypto = require('crypto');                      // verify request from slack is from slack with hmac-256
-var fs = require('fs');                              // i dunno, I like files, and chickens
-var querystring = require('querystring');
+var querystring = require('querystring');            // Parse urlencoded body
 
 module.exports.remember = function(event, context, callback) {
-    var body = querystring.parse(event.body);
-    var response = {
-        statusCode: 200,
-        headers: {
-            'Content-type': 'application/json'
-        },
-        body: JSON.stringify({
-            'text': 'hello ' + body.user_name + '!',
-            // 'attachments': [{ 'text': 'yo' }] // This an example of how attachments are formated
-        })
-    };
+    var body = querystring.parse(event.body);        // Parse urlencoded body
+    var response = {statusCode: 403};                // until request is authentically varified as from slack
+    if(varify.request(event)){                       // make sure this request comes from slack
+        response = {
+            statusCode: 200,
+            headers: {
+                'Content-type': 'application/json'   // content type for richer responses beyound just text
+            },
+            body: JSON.stringify({
+                'text': 'hello ' + body.user_name + '!',
+                // 'attachments': [{ 'text': 'yo' }] // This an example of how attachments are formated
+            })
+        };
+    }
     callback(null, response);
 };
 
-/*
+
 var varify = {
     slack_sign_secret: process.env.SLACK_SIGNING_SECRET,
-    request: function(expectedBody){
-
+    request: function(event){
+        var timestamp = event.headers['X-Slack-Request_Timestamp'];            // nonce from slack to have an idea
+        if(Math.abs(new Date().getTime() - timestamp > 60 * 5)){return false;} // make sure request isn't a duplicate
+        var computedSig = crypto.createHmac('sha256', verify.slack_sign_secret).update('v0:' + timestamp + ':' + event.body).digest('hex');
+        return crypto.timingSafeEqual(Buffer.from(event.headers['X-Slack-Signature'], 'utf8'), Buffer.from(computedSig ,'utf8'));
     }
 };
-*/

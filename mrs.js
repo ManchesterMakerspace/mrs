df82ddeeed769c1f09aa65a38f3d838a53cdc374
PaulBeaudet <inof8or@gmail.com>
2018-08-29 17:00:03 -0400

    get notes back

--- mrs.js
+++ mrs.js
@@ -8,7 +8,7 @@ module.exports.remember = function(event, context, callback) {
     var body = querystring.parse(event.body);        // Parse urlencoded body
     var response = {statusCode: 403};                // until request is authentically varified as from slack
     if(varify.request(event)){                       // make sure this request comes from slack
-        mongo.storeData(body, function whenStored(error, data){
+        mongo.handleRequest(body, function whenHandled(error, data){
             response = {
                 statusCode: 200,
                 headers: {'Content-type': 'application/json'},   // content type for richer responses beyound just text
@@ -16,7 +16,11 @@ module.exports.remember = function(event, context, callback) {
             if(error){
                 response.body = JSON.stringify({'text' : error.name + ': ' + error.code});
             } else {
-                response.body = JSON.stringify({'text' : body.user_name + ' just recorded a note- ' + body.text});
+                if(data.text){
+                    response.body = JSON.stringify({'text' : data.text});
+                } else {
+                    response.body = JSON.stringify({'text' : body.user_name + ' just recorded a note- ' + body.text});
+                }
             }
             callback(null, response);
         });
@@ -24,6 +28,7 @@ module.exports.remember = function(event, context, callback) {
 };
 
 var mongo = {
+    dbName: 'makerspace_testing',
     ObjectID: require('mongodb').ObjectID,
     client: require('mongodb').MongoClient,
     connectAndDo: function(connected, failed){         // url to db and what well call this db in case we want multiple
@@ -32,30 +37,44 @@ var mongo = {
             else if(client){connected(client);} // passes client connection object so databasy things can happen
         });
     },
-    storeData: function(body, onStore){
-        console.log('store data called');
+    handleRequest: function(body, onHandled){
         mongo.connectAndDo(function onConnect(client){ // if there is an error skip right to onStore function to hand error and respond
             var textArray = body.text.split(':');      // split arguments with colons
-            var member = 'not sure';
-            var note = body.text;     // default to full text given no split
+            var contact = body.text;  //.toLowerCase();
+            var note = null;          // default to full text given no split
             if(textArray.length > 1){ // given we got two arguments split them into member and note fields
-                member = textArray[0];
+                contact = textArray[0];
                 note = textArray[1];
+                client.db(mongo.dbName).collection('notes').insertOne({
+                    _id: new mongo.ObjectID(),
+                    timestamp: new Date().getTime(),
+                    nodeTakerId: body.user_id,
+                    noteTaker: body.user_name,
+                    forContact: contact, // this could be a current member, potential member, teacher, or community partner
+                    note: note,
+                    channel: body.channel_name,
+                    channelId: body.channel_id
+                }, function whenDone(error, data){
+                    onHandled(error, data);
+                    client.close();
+                });
+            } else { // given just one argument is passed a search is assumed
+                var cursor = client.db(mongo.dbName).collections('notes').find({forContact: contact});
+                var collatedResult = '';
+                mongo.stream(cursor, client, collatedResult, onHandled);
             }
-            client.db('makerspace_testing').collection('notes').insertOne({
-                _id: new mongo.ObjectID(),
-                timestamp: new Date().getTime(),
-                nodeTakerId: body.user_id,
-                noteTaker: body.user_name,
-                forMember: member,
-                note: note,
-                channel: body.channel_name,
-                channelId: body.channel_id
-            }, function whenDone(error, data){
-                onStore(error, data);
-                client.close();
-            });
         }, onStore); // in fail to connect case pass onStore function to handle error
+    },
+    stream: function(cursor, client, collatedResult, onHandled){
+        cursor.next(function onNote(error, doc){ // syncronously/recursively move through cursor results because we have nothing else todo
+            if(doc){
+                collatedResult += doc.note + '\n'; // collate relault to be returned
+                mongo.stream(cursor, client, collatedResult, onHandled);
+            } else {
+                onHandled(error, {text: collatedResult});
+                client.close();
+            }
+        });
     }
 };
 
